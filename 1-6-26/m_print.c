035fb66048bcd2ee33b3288fe4429ca7a7b21fff
Rahulbiradar9 <rahulbiradar7134@gmail.com>
2026-06-01 11:30:33 +0530

    C-DS

---
+++ 1-6-26/m_print.c
@@ -0,0 +1,14 @@
+// #include<stdio.h>
+
+// int main()
+// {
+//     int a=10;
+//     printf(" %d %d %d", a , a++ , ++a);
+// }
+
+#include<stdio.h>
+
+int main(){
+    int x = 5;
+    printf("%d" , x+++x);
+}
